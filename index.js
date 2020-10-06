ce6d9e0f2ede7fec5935355f424fd9fd4fe334e4
Vladyslav Ternovoi <vladislav.ternovoy@meliorgames.com>
2020-10-07 00:52:25 +0300

    [ADD] added new link

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("index");
 });
+app.get("/info", (req, res) => {
+    res.send("info");
+  });
 
 
 app.listen({ port: process.env.PORT }, () => console.log("running"));
